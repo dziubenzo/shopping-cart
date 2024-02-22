dd41fbc76833c3ce4e42e557099e97304acfcdc9
dziubenzo <dziubenzo@gmail.com>
2024-02-22 16:00:51 +0100

    Create AppTitle component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,23 @@ import Products from './Products';
 import Cart from './Cart';
 import ErrorPage from './ErrorPage';
 import css from './App.module.scss';
+import AppTitle from './AppTitle';
 
 function App({ route }) {
   return (
     <>
-      <h2>Header</h2>
-      {route === 'home' && <Home />}
-      {route === 'products' && <Products />}
-      {route === 'cart' && <Cart />}
-      {route === 'error' && <ErrorPage />}
-      <h2>Footer</h2>
+      <header>
+        <AppTitle />
+      </header>
+      <main>
+        {route === 'home' && <Home />}
+        {route === 'products' && <Products />}
+        {route === 'cart' && <Cart />}
+        {route === 'error' && <ErrorPage />}
+      </main>
+      <footer>
+        <h2>Footer</h2>
+      </footer>
     </>
   );
 }
---
+++ src/components/AppTitle.jsx
@@ -0,0 +1,11 @@
+import css from './AppTitle.module.scss';
+
+function AppTitle() {
+  return (
+    <>
+      <h2 className={css.appTitle}>Shopping Cart</h2>
+    </>
+  );
+}
+
+export default AppTitle;
