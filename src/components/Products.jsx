3054c1f41ce16957e156c56261ed13d0ed6af1c8
dziubenzo <dziubenzo@gmail.com>
2024-02-23 13:37:42 +0100

    Show all products on Products page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,40 @@
 import PropTypes from 'prop-types';
+import ProductCard from './ProductCard';
 
 function Products({ products, error, loading }) {
+  // Create ProductCard components for all products
+  function createCards() {
+    let productArray = [];
+    for (const product of products) {
+      productArray.push(
+        <ProductCard
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          image={product.image}
+          price={product.price}
+          description={product.description}
+          rate={product.rating.rate}
+          count={product.rating.count}
+        />,
+      );
+    }
+    return productArray;
+  }
+
+  function renderProducts() {
+    return (
+      <>
+        <h1>All Products ({products.length})</h1>
+        <div>{createCards()}</div>
+      </>
+    );
+  }
   return (
     <>
       {error && <h1>{error.message}</h1>}
       {loading && <h1>Loading products...</h1>}
-      {products && <h1>All Products ({products.length})</h1>}
+      {products && renderProducts()}
     </>
   );
 }
