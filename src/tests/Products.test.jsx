3f412e8c905f4146713ece1a351f93048de60766
dziubenzo <dziubenzo@gmail.com>
2024-02-27 12:19:56 +0100

    Write ProductCard tests

--- src/tests/Products.test.jsx
+++ src/tests/Products.test.jsx
@@ -1,7 +1,8 @@
-import { it, expect, vi, describe } from 'vitest';
+import { it, expect, vi, describe, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Products from '../components/Products';
+import ProductCard from '../components/ProductCard';
 
 const sampleProducts = [
   {
@@ -52,5 +53,92 @@ describe('Products component', () => {
     await user.click(addToCartButton);
 
     expect(mockHandleCart).toHaveBeenCalled();
+  });
+});
+
+describe('ProductCard component', () => {
+  let user;
+  let input;
+
+  beforeEach(() => {
+    const mockHandleCart = vi.fn();
+    user = userEvent.setup();
+
+    render(
+      <ProductCard
+        id={sampleProducts[0].id}
+        title={sampleProducts[0].title}
+        image={sampleProducts[0].image}
+        price={sampleProducts[0].price}
+        description={sampleProducts[0].description}
+        rate={sampleProducts[0].rating.rate}
+        count={sampleProducts[0].rating.count}
+        handleCart={mockHandleCart}
+      />,
+    );
+
+    input = screen.getByRole('spinbutton');
+  });
+
+  it('shows input value if a correct one is typed', async () => {
+    await user.type(input, '99');
+
+    expect(input.value).toEqual('99');
+  });
+
+  it.each([['-5'], ['100'], ['string']])(
+    'does not allow incorrect values to be typed (%s)',
+    async (value) => {
+      await user.type(input, value);
+
+      expect(input.value).not.toEqual(value);
+    },
+  );
+
+  it('increases quantity when button clicked', async () => {
+    const incrementButton = screen.getByAltText('Increase Quantity Icon');
+    await user.click(incrementButton);
+    await user.click(incrementButton);
+    await user.click(incrementButton);
+
+    expect(input.value).toEqual('3');
+  });
+
+  it('does not allow quantity to go above max', async () => {
+    const incrementButton = screen.getByAltText('Increase Quantity Icon');
+    await user.type(input, '98');
+    await user.click(incrementButton);
+    await user.click(incrementButton);
+    await user.click(incrementButton);
+    await user.click(incrementButton);
+
+    expect(input.value).toEqual('99');
+  });
+
+  it('decreases quantity when button clicked', async () => {
+    const incrementButton = screen.getByAltText('Increase Quantity Icon');
+    const decrementButton = screen.getByAltText('Decrease Quantity Icon');
+    await user.click(incrementButton);
+    await user.click(incrementButton);
+    await user.click(decrementButton);
+
+    expect(input.value).toEqual('1');
+  });
+
+  it('does not allow quantity to go below min', async () => {
+    const decrementButton = screen.getByAltText('Decrease Quantity Icon');
+    await user.click(decrementButton);
+    await user.click(decrementButton);
+    await user.click(decrementButton);
+
+    expect(input.value).toEqual('0');
+  });
+
+  it('resets quantity to 0 after clicking the Add To Cart button', async () => {
+    const addToCartButton = screen.getByAltText('Add To Cart Icon');
+    await user.type(input, '55');
+    await user.click(addToCartButton);
+
+    expect(input.value).toEqual('0');
   });
 });
